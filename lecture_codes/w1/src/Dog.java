750ed0b75dceae42d9a66e02459d46674fe4436a
Seongmin Na <seongminna0627@gmail.com>
2022-10-10 22:20:14 -0700

    More notes on non-static vs.static

--- lecture_codes/w1/src/Dog.java
+++ lecture_codes/w1/src/Dog.java
@@ -1,5 +1,6 @@
 public class Dog {
     public int weightInPounds;
+    public static String binomen = "Canis famailiaris";
     // How big a dog is
     public Dog(int w){
         weightInPounds = w;
@@ -13,6 +14,22 @@ public class Dog {
             System.out.println("woooooof!");
         }
     }
+    public static Dog maxDog(Dog d1, Dog d2){
+        if (d1.weightInPounds >= d2.weightInPounds){
+            return d1;
+        }else{
+            return d2;
+        }
+    }
+    public Dog maxDog(Dog d2){
+        if (this.weightInPounds >= d2.weightInPounds){
+            return this;
+        }
+        else{
+            return d2;
+        }
+    }
+
 }
 /**
  * Roughly speaking,
--- lecture_codes/w1/src/DogLauncher.java
+++ lecture_codes/w1/src/DogLauncher.java
@@ -3,7 +3,15 @@ public class DogLauncher {
     public static void main(String[] args){
         Dog aRandomDog = new Dog(51);
         aRandomDog.makeNoise();
+        Dog d1 = new Dog(51);
+        Dog d2 = new Dog(100);
+        Dog bigger = Dog.maxDog(d1, d2);
+        bigger.makeNoise();
+        Dog larger = d2.maxDog(d1);
+        larger.makeNoise();
+
     }
+
 }
 /**
  1. To run a class, we must define a main method.
@@ -12,4 +20,10 @@ public class DogLauncher {
  * static == anything that is constant throughout the class.
  * non-static == anything that can be instaniated and can be used as an instance.
 
+
+ 2. Static vs Non-Static
+    - Static methods are invoked using the class name, e.g. Dog.makeNoise();
+    - Instance methods are invoked using an instance name, e.g. maya.makeNoise();
+    - Non-static members cannot be invoked using class name: Dog.makeNoise() NOOOOOO!!!
+    - Static methods must access instance variables via a specific instance, e.g. d1.
  */
