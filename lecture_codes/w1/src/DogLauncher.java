1c2bca896cee8ead42b79492ee7576e99ca4d514
Seongmin Na <seongminna0627@gmail.com>
2022-10-10 21:43:33 -0700

    The very first version of Dog class

---
+++ lecture_codes/w1/src/DogLauncher.java
@@ -0,0 +1,12 @@
+/** The DogLauncher class will 'test drive' the Dog class*/
+public class DogLauncher {
+    public static void main(String[] args){
+        Dog.makeNoise();
+    }
+}
+/**
+ 1. To run a class, we must define a main method.
+    - Not all classes have a main method!
+
+
+ */
