d09fd9c5db0581d0776e2ecee53535710f9e9da2
Seongmin Na <seongminna0627@gmail.com>
2022-10-10 21:56:29 -0700

    Dog class with non-static methods/without initialization

--- lecture_codes/w1/src/Dog.java
+++ lecture_codes/w1/src/Dog.java
@@ -1,5 +1,18 @@
 public class Dog {
-    public static void makeNoise(){
-        System.out.println("Bark!");
+    public int weightInPounds;
+    // How big a dog is
+    public void makeNoise(){
+        if (weightInPounds < 10) {
+            System.out.println("yip!");
+        }else if (weightInPounds < 30) {
+            System.out.println("bark.");
+        }else {
+            System.out.println("woooooof!");
+        }
     }
 }
+/**
+ * Roughly speaking,
+ * static == anything that is constant throughout the class.
+ * non-static == anything that can be instaniated and can be used as an instance.
+ */
