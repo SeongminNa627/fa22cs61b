3fd6d7c7cfc313fa7c47eae62d22c1daf8205338
Seongmin Na <seongminna0627@gmail.com>
2022-10-10 21:59:43 -0700

    Dog class with the constructer

--- lecture_codes/w1/src/DogLauncher.java
+++ lecture_codes/w1/src/DogLauncher.java
@@ -1,12 +1,15 @@
 /** The DogLauncher class will 'test drive' the Dog class*/
 public class DogLauncher {
     public static void main(String[] args){
-        Dog.makeNoise();
+        Dog aRandomDog = new Dog(51);
+        aRandomDog.makeNoise();
     }
 }
 /**
  1. To run a class, we must define a main method.
     - Not all classes have a main method!
-
+ * Roughly speaking,
+ * static == anything that is constant throughout the class.
+ * non-static == anything that can be instaniated and can be used as an instance.
 
  */
