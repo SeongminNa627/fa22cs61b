0ca6915337c826975070cb3fe85447af9f24a8c0
Seongmin Na <seongminna0627@gmail.com>
2022-10-11 21:42:15 -0700

    ArrayMax with for loop

--- hw1/src/ArrayMax.java
+++ hw1/src/ArrayMax.java
@@ -1,15 +1,22 @@
 public class ArrayMax {
     public static int max(int[] arr){
-     int i = 0;
-     int max = arr[i];
-     while (i < arr.length - 1){
-         int next = arr[i + 1];
-         if (max < next){
-             max = next;
-         }
-         i = i + 1;
-         }
-     return max;
+//     int i = 0;
+//     int max = arr[i];
+//     while (i < arr.length - 1){
+//         int next = arr[i + 1];
+//         if (max < next){
+//             max = next;
+//         }
+//         i = i + 1;
+//         }
+//     return max;
+        int max = arr[0];
+        for (int i = 0; i < arr.length - 1; i = i + 1){
+            if (max < arr[i + 1]){
+                max = arr[i + 1];
+            }
+        }
+        return max;
     }
     public static void main(String[] args){
         int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6};
