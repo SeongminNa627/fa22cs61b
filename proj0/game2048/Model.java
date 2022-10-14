a55d2173637c2077022a9ed5e3523def751ba4a7
Seongmin Na <seongminna0627@gmail.com>
2022-10-13 22:53:51 -0700

    Implement maxTileExists

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -155,6 +155,11 @@ public class Model extends Observable {
      */
     public static boolean maxTileExists(Board b) {
         // TODO: Fill in this function.
+        for (Tile t: b){
+            if (t != null && t.value() == MAX_PIECE){
+                return true;
+            }
+        }
         return false;
     }
 
