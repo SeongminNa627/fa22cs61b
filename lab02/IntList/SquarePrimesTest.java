23af4a876e2152b6582fafcfa5a5793adeedb4bd
Seongmin Na <seongminna0627@gmail.com>
2022-11-05 19:49:19 -0700

    complete w2. Debugging practice

--- lab02/IntList/SquarePrimesTest.java
+++ lab02/IntList/SquarePrimesTest.java
@@ -17,4 +17,25 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+    @Test
+    public void testSquarePrimesChangedTwice(){
+        IntList L = IntList.of(2,9,8,12,11);
+        boolean changed = IntListExercises.squarePrimes(L);
+        assertEquals("4 -> 9 -> 8 -> 12 -> 121", L.toString());
+        assertTrue(changed);
+    }
+    @Test
+    public void testSquarePrimesLast(){
+        IntList L = IntList.of(4,9,8,12,11);
+        boolean changed = IntListExercises.squarePrimes(L);
+        assertEquals("4 -> 9 -> 8 -> 12 -> 121", L.toString());
+        assertTrue(changed);
+    }
+    @Test
+    public void testSquarePrimesAllPrimes(){
+        IntList L = IntList.of(2,3,5,7,11);
+        boolean changed = IntListExercises.squarePrimes(L);
+        assertEquals("4 -> 9 -> 25 -> 49 -> 121", L.toString());
+        assertTrue(changed);
+    }
 }
