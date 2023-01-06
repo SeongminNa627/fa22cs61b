7ccfe28a1590c003b1ff347f46447ab2c9b5094e
Seongmin Na <seongminna0627@gmail.com>
2023-01-05 18:19:59 -0800

    Major implementation were completed

--- hw2/hw2/PercolationFactory.java
+++ hw2/hw2/PercolationFactory.java
@@ -4,4 +4,5 @@ public class PercolationFactory {
     public Percolation make(int N) {
         return new Percolation(N);
     }
+    public PercolationWQFUF makeQF(int N){ return new PercolationWQFUF(N);}
 }
