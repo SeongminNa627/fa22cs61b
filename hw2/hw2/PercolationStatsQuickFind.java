5aecd45a53dcf800de1ad330707b8e46d977d3b2
Seongmin Na <seongminna0627@gmail.com>
2023-01-05 18:22:04 -0800

    Percolation implemented using Quick Find approach

---
+++ hw2/hw2/PercolationStatsQuickFind.java
@@ -0,0 +1,37 @@
+package hw2;
+import java.util.*;
+import edu.princeton.cs.algs4.*;
+public class PercolationStatsQuickFind {
+    private int N;
+    private int T;
+    private double sumOfFraction;
+    private double sumOfSquaredFraction;
+    private double[] percSamples;
+
+    public PercolationStatsQuickFind(int N, int T, PercolationFactory pf){
+        if (N <= 0 || T <= 0){
+            throw new IllegalArgumentException();
+        }
+        percSamples = new double[T];
+        for (int i = 0; i < T; i ++){
+            PercolationWQFUF p = pf.makeQF(N);
+            while (!p.percolates()){
+                p.open(StdRandom.uniform(N), StdRandom.uniform(N));
+            }
+            percSamples[i] = ((double) p.numberOfOpenSites())/(N*N);
+        }
+
+    }
+    public double mean(){
+        return StdStats.mean(percSamples);
+    }
+    public double stddev(){
+        return StdStats.stddev(percSamples);
+    }
+    public double confidenceLow(){
+        return this.mean() - (1.96 * this.stddev())/T;
+    }
+    public double confidenceHigh(){
+        return this.mean() + (1.96 * this.stddev())/T;
+    }
+}
